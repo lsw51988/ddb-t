0dd3f4fa76d43f7cb51353c43018bff45621cc1c
少文 <lishaowen@dowedo.com.cn>
2018-02-09 17:06:55 +0800

    init

---
+++ app/Http/Controllers/Api/Member/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\Member;
+
+use App\Model\Members;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * 用户首次进入逻辑
+     * 1.没有该用户，注册
+     * 2.返回该用户信息
+     */
+    public function index(Request $request)
+    {
+        print_r($request->input());
+        if ($member = Members::where("nickName", "小红")->first()) {
+            print_r($member->toArray());
+        }
+    }
+}
