0c94c1fa404fe593e12c439ec812754bb259b448
少文 <lishaowen@dowedo.com.cn>
2018-02-26 10:23:57 +0800

    修复session无法记录bug

--- app/Http/Controllers/Api/Member/IndexController.php
+++ app/Http/Controllers/Api/Member/IndexController.php
@@ -62,7 +62,6 @@ class IndexController extends Controller
      */
     public function verifyCaptcha(Request $request)
     {
-        return $this->success(session('captcha'));
         $captcha = $request->input('captcha');
         if (session('captcha') == $captcha) {
             return $this->success();
